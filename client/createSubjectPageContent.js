c4840ac505ce99d59946f36c1188ca408f8224be
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-05-10 14:40:18 -0400

    Condenced subject page creation modules

--- client/createSubjectPageContent.js
+++ client/createSubjectPageContent.js
@@ -1,9 +1,7 @@
-export default function(state){
-  var subjectId = state.ui.selectedItem;
+export default function(state, actions){
+  var subjectId = state.ui.selectedSubject;
   var subject = state.db[subjectId];
-  console.log(subject);
-  var containerId = subject._id.split('.').slice(-2,-1);
-  var container = state.db[containerId];
+  var container = state.db[subject.containerId];
   if( !container ){
     container = {
       name: 'reality'
@@ -13,11 +11,114 @@ export default function(state){
 
   var pageContent = {
     title: subject.name,
-    description: `${subject.name} is a ${subject.type} located in ${container.name}`,
-    facts: {
+    description: [
+      subject.name,
+      ' is a ',
+      {
+        tag: 'a',
+        href: '#',
+        //onclick: actions.selectSubject('l.type.' + subject.type),
+        onclick: function(){
+          console.log('test');
+        },
+        text: subject.type
+      },
+      ', located in ',
+      container.type,
+      ' ',
+      container.name
+    ],
+    contains: [],
+    info: {
+      type: subject.type,
       characteristics: subject.info
-    }
+    },
+    subject: subject
   };
 
-  return pageContent;
+  //subject.contains.forEach(function(containedSubjectID){
+  //  var containedSubject = state.db[containedSubjectID];
+  //  pageContent.contains.push('');
+  //});
+
+
+
+  var mkInfoBox = function(pageContent){
+    var infoBoxConfig = {
+      tag: 'div',
+      class: 'infoBox',
+      children: [
+        {
+          tag: 'span',
+          text: 'Type: ' + pageContent.info.type
+        }
+      ]
+    };
+
+    if(pageContent.info.characteristics){
+      console.log(pageContent.info.characteristics);
+      _.forIn(pageContent.info.characteristics, function(value, name){
+        infoBoxConfig.children.push({
+          tag: 'div',
+          text: _.upperFirst(name) +': '+ value
+        });
+
+      });
+    }
+
+    return infoBoxConfig;
+  };
+
+  var mkDescription = function(pageContent){
+    var description = {
+      tag: 'div',
+      class: 'section descriptionSection',
+      children: pageContent.description
+    };
+
+    return description;
+  };
+
+  var mkRaw = function(pageContent){
+    var raw = {
+      tag: 'div',
+      class: 'section rawSection',
+      children: [
+        {
+          tag: 'pre',
+          text: JSON.stringify(pageContent, null, 4)
+        }
+      ]
+    };
+
+    return raw;
+  };
+
+  var pageConfig = {
+    tag: 'div',
+    class: 'infoPage',
+    children: [
+      {
+        tag: 'div',
+        class: 'pageTitleBar',
+        children: [
+          {
+            tag: 'h1',
+            text: pageContent.title
+          }
+        ]
+      },
+      {
+        tag: 'div',
+        class: 'pageBody',
+        children: [
+          mkDescription(pageContent),
+          mkRaw(pageContent)
+        ]
+      },
+      mkInfoBox(pageContent)
+    ]
+  };
+
+  return pageConfig;
 }

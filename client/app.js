424850da5f3fad33742b83f74769002f6e93955a
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-05-12 16:08:19 -0400

    Added documentation generation

--- client/app.js
+++ client/app.js
@@ -1,3 +1,10 @@
+/**
+ * This is the App
+ * @fileOverview This the starting point for the application.
+ * @author Keith Showalter {@link https://github.com/kshowalter}
+ * @version 0.1.0
+ */
+
 import _ from 'lodash';
 
 import Chance from 'chance';
@@ -7,17 +14,35 @@ var createStore = require('redux').createStore;
 
 var seed = 'PhoebeWillow';
 
+/**
+ * Creates an instance of Circle.
+ *
+ * @constructor
+ * @this {Circle}
+ * @param {number} r The desired radius of the circle.
+ */
+function Circle(r) {
+  /** @private */ this.radius = r;
+  /** @private */ this.circumference = 2 * Math.PI * r;
+}
 
+
+/** @module */
 import mkInitState from './mkInitState';
 var initState = mkInitState(seed);
 //console.log(initState);
 
+/** Create the store */
 var store = createStore(reducer, initState);
 
+/** @module */
 import Actions from './actions';
 var actions = Actions(store);
 
-import View from './view';
+/** @module view */
+var View = './view';
+//import View from './view';
+
 var view = View(store, actions);
 
 var selectedSubject = sessionStorage.getItem('selectedSubject');
@@ -26,6 +51,7 @@ if( selectedSubject ){
 }
 
 
+/** Anonymous function that runs when the store is updated. */
 store.subscribe(function(){
   var state = store.getState();
   window.state = state; // DEVMODE

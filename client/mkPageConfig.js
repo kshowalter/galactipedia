5bcebf9ece3ace0ebe1cdb689265093ee6ef7015
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-05-16 11:16:26 -0400

    Added page not found function

--- client/mkPageConfig.js
+++ client/mkPageConfig.js
@@ -3,11 +3,23 @@ import mkListPageConfig from './mkListPageConfig';
 
 export default function(state, actions){
   var pageType = state.ui.selectedSubject.split('.')[0];
+  var subject = state.db[state.ui.selectedSubject];
+  console.log(subject);
+  if( subject === undefined ){
+    console.log('subject not found');
+    setTimeout( function(){
+      actions.selectSubject('l');
+    }, 3000);    
+    return {
+      tag: 'div',
+      text: 'page not found'
+    };
+  }
 
-  if( pageType === 'u' ){
-    console.log('rendering Universe page ', state.ui.selectedSubject);
+  if( pageType === 'l' ){
+    console.log('rendering location page ', state.ui.selectedSubject);
     return mkSubjectPageConfig(state, actions);
-  } else if( pageType === 'u' ){
+  } else if( pageType === 'o' ){
     console.log('rendering Universe page ', state.ui.selectedSubject);
     return mkListPageConfig(state, actions);
   }

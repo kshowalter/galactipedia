a487c4d50c67b273262b00fdbf6ff6fbb92f7492
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-05-25 17:05:19 -0400

    Moved some info to kstore

--- client/universe/define/star.js
+++ client/universe/define/star.js
@@ -1,10 +1,11 @@
 // http://www.atlasoftheuniverse.com/startype.html
-
+import Kstore from 'kstore';
 
 export const info = {
   // https://en.wikipedia.org/wiki/Stellar_classification
-  classifications: {
-    'O': {
+  classifications: Kstore([
+    {
+      'classification': 'O',
       'Color': 'Blue',
       'Aprox surface temp': 'over 25,000 K',
       'Average mass':0,
@@ -12,7 +13,8 @@ export const info = {
       'Average Luminocity': 1400000,
       'Characteristics': 'Singly ionized helium lines (H I) either in emission or absorption. Strong UV continuum.'
     },
-    'B': {
+    {
+      'classification': 'B',
       'Color': 'Blue',
       'Aprox surface temp': '11,000 - 25,000 K',
       'Average mass':8,
@@ -20,7 +22,8 @@ export const info = {
       'Average Luminocity': 20000,
       'Characteristics': 'Neutral helium lines (H II) in absorption.'
     },
-    'A': {
+    {
+      'classification': 'A',
       'Color': 'Blue',
       'Aprox surface temp': '7,500 - 11,000 K',
       'Average mass': 3.2,
@@ -28,7 +31,8 @@ export const info = {
       'Average Luminocity': 80,
       'Characteristics': 'Hydrogen (H) lines strongest for A0 stars, decreasing for other A\'s.'
     },
-    'F': {
+    {
+      'classification': 'F',
       'Color': 'Blue to White',
       'Aprox surface temp': '6,000 - 7,500 K',
       'Average mass': 1.7,
@@ -36,7 +40,8 @@ export const info = {
       'Average Luminocity': 6,
       'Characteristics': 'Ca II absorption. Metallic lines become noticeable.'
     },
-    'G': {
+    {
+      'classification': 'G',
       'Color': 'White to Yellow',
       'Aprox surface temp': '5,000 - 6,000 K',
       'Average mass': 1.1,
@@ -44,7 +49,8 @@ export const info = {
       'Average Luminocity': 1.2,
       'Characteristics': 'Absorption lines of neutral metallic atoms and ions (e.g. once-ionized calcium).'
     },
-    'K': {
+    {
+      'classification': 'K',
       'Color': 'Orange to Red',
       'Aprox surface temp': '3,500 - 5,000 K',
       'Average mass': 0.8,
@@ -52,7 +58,8 @@ export const info = {
       'Average Luminocity': 0.4,
       'Characteristics': 'Metallic lines, some blue continuum.'
     },
-    'M': {
+    {
+      'classification': 'M',
       'Color': 'Red',
       'Aprox surface temp': 'under 3,500 K',
       'Average mass': 0.3,
@@ -60,7 +67,7 @@ export const info = {
       'Average Luminocity': 0.04,
       'Characteristics': 'Some molecular bands of titanium oxide.'
     }
-  }
+  ])
 };
 
 
@@ -78,8 +85,11 @@ export function summarize(state, request){
     name: request.name,
     info: request.info
   };
-  _.keys(info.classifications[summary.info.classification]).forEach(function(name){
-    summary.info[name] = info.classifications[summary.info.classification][name];
+  var classificationInfo = info.classifications.findOne({
+    classification: summary.info.classification
+  });
+  _.keys(classificationInfo).forEach(function(name){
+    summary.info[name] = classificationInfo[name];
   });
   state.db[summary._id] = summary;
 

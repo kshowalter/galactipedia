ed219efd5c70b812f3aed855ca104a5ecd8bc4f5
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-03-25 00:00:22 -0400

    Misc updates to build environment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
+var path = require('path');
 var webpack = require('webpack');
 //var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
 
 module.exports = {
   entry: {
@@ -13,66 +13,22 @@ module.exports = {
     filename: '[name].js'
   },
 
+  //plugins: [
+  //  new webpack.DefinePlugin({
+  //    'process.env.NODE_ENV': JSON.stringify('dev')
+  //  })
+  //],
+
   module: {
     loaders: [
       {
         test: /\.js$/,
-        //exclude: /node_modules/,
         include: /client/,
-        loader: 'babel-loader',
-        query: {
-          // https://github.com/babel/babel-loader#options
-          cacheDirectory: true,
-          presets: ['es2015']
-        }
+        //exclude: /node_modules/,
+        loader: 'babel-loader'
       }
     ]
+
   }
 
 };
-
-/*
-module.exports = {
-  entry: {
-    app: './client/app.js'
-  },
-  output: {
-    path: './public/',
-    filename: '[name].js'
-    //inject: true
-    //devtoolLineToLine: true,
-  },
-  //devtool: 'eval',
-  //devtool: 'eval-source-map',
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('dev')
-    })
-    //new HtmlWebpackPlugin({
-    //  title: 'test',
-    //  //filename: 'assets/admin.html',
-    //  template: 'client/index.ejs',
-    //  inject: true
-    //})
-  ],
-  module: {
-    loaders: [
-      {
-        //test: /\.es6$/,
-        test: /\.js$/,
-        //test: path.join(__dirname, 'src_browser'),
-        //exclude: /(node_modules)/,
-        //exclude: /node_modules/,
-        include: /client/,
-        loader: 'babel-loader',
-        query: {
-          // https://github.com/babel/babel-loader#options
-          cacheDirectory: true,
-          presets: ['es2015']
-        }
-      }
-    ]
-  }
-};
-*/

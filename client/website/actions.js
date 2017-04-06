27b4b06ae09e3fa58ae5e018539a8833f2c77e26
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-04-05 23:57:17 -0400

    Protecting internal functions in actions.js.

--- client/website/actions.js
+++ client/website/actions.js
@@ -1,8 +1,8 @@
 export default function(store, custom_actions){
   var actions = {
-    store: store,
-    dispatch: function(action_config){
-      this.store.dispatch(action_config);
+    __store: store,
+    __dispatch: function(action_config){
+      this.__store.dispatch(action_config);
     },
   };
 
@@ -10,7 +10,7 @@ export default function(store, custom_actions){
     actions[action_name] = function(){
       var action_making_function = custom_actions[action_name];
       console.log('action: ', action_name);
-      this.dispatch( action_making_function(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4] ));
+      this.__dispatch( action_making_function(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4] ));
     };
   }
 
